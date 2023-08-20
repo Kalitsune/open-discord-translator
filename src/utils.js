53c26a07e1a4a28132258dbf6401eafff274beca
-ˏˋ Maple ˊˎ <fanny@kalitsune.cloud>
2023-08-20 16:30:08 +0200

    🇰🇷 Fixed Korean Flag

--- src/utils.js
+++ src/utils.js
@@ -5,6 +5,7 @@ module.exports = {
     getFlagEmoji (countryCode) {
         // en is not a flag so pick gb/us instead
         if (countryCode === 'ja') countryCode = 'jp';
+        if (countryCode === 'ko') countryCode = 'kr';
         if (countryCode === 'en') countryCode = ['gb','us'][Math.floor(Math.random()*2)];
         if (countryCode === 'en-GB') countryCode = 'gb';
         if (countryCode === 'en-US') countryCode = 'us';
